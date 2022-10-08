aff45295dd93a46db9a768b2ea92bedb9dda5eea
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-08 15:54:07 +0500

    feat: make about me section

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,10 +1,24 @@
 import './AboutMe.css';
+ import Avatar from '../../images/avatar.jpg';
 
 function AboutMe() {
   return (
-    <div className='AboutMe'>
-      AboutMe
-    </div>
+    <section className='about-me'>
+      <h2 className='about-me__heading'>Студент</h2>
+      <div className='about-me__contain'>
+        <div className='about-me__info'>          
+          <h3 className='about-me__name'>Екатерина</h3>
+          <p className='about-me__profession'>Фронтенд-разработчик, 37 лет</p>
+          <p className='about-me__description'>
+            Я родилась и живу в Екатеринбурге, закончила лечебный факультет УГМА. Люблю спорт, занимаюсь скалолазанием и плаванием. Более 10 лет проработала врачем-рентгенологом. Недавно начала кодить. После того, как прошла курс по веб-разработке, ушла с постоянной работы.
+          </p>
+          <a className='about-me__link' href='https://github.com/EpiphES' rel='noreferrer' target='_blank'>
+            Github
+          </a>    
+        </div>
+        <img src={Avatar} className='about-me__photo' alt='мое фото' />   
+      </div>
+    </section>
   );
 }
 
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,7 @@ import '../Footer/Footer';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from'../Techs/Techs';
-import '../AboutMe/AboutMe';
+import AboutMe from '../AboutMe/AboutMe';
 import '../Portfolio/Portfolio';
 
 function Main() {
@@ -14,6 +14,7 @@ function Main() {
       <Promo />
       <AboutProject />
       <Techs />
+      <AboutMe />
     </div>
   );
 }
