23cfb6465f4d48687e1b6e551328adb3e720db7f
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-08 16:17:03 +0500

    feat: make portfolio section

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,9 +2,30 @@ import './Portfolio.css';
 
 function Portfolio() {
   return (
-    <div className="Portfolio">
-      
-    </div>
+    <section className='portfolio'>
+			<h2 className='portfolio__title'>Портфолио</h2>
+			<ul className='portfolio__links'>
+				<li>
+          <a className="portfolio__link" href="https://epiphes.github.io/how-to-learn/index.html" rel="noreferrer" target="_blank">
+            Статичный сайт
+            <span   className="portfolio__arrow">↗</span>
+				  </a>
+        </li>
+        <li>
+          <a className="portfolio__link" href="https://epiphes.github.io/russian-travel/index.html" rel="noreferrer" target="_blank">
+            Адаптивный сайт
+            <span   className="portfolio__arrow">↗</span>
+				  </a>
+        </li>
+        <li>
+          <a className="portfolio__link" href="https://epiphes.github.io/mesto-react" rel="noreferrer" target="_blank">
+            Одностраничное приложение
+            <span   className="portfolio__arrow">↗</span>
+				  </a>
+        </li>
+				
+			</ul>
+		</section >
   );
 }
 
