7b866497532bdfc6f4844625c889e98c67906bcd
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-10 11:52:39 +0500

    feat: login page

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,107 @@
 import './Login.css';
+import { Link } from 'react-router-dom';
+
+import useForm from '../../utils/useForm';
+
+import Logo from '../Logo/Logo';
+import AuthForm from '../AuthForm/AuthForm';
 
 function Login() {
+  const initialFormValues = {
+    name: {
+      value: '',
+      error: '',
+      isValid: true,
+    },
+    email: {
+      value: '',
+      error: '',
+      isValid: true,
+    },
+    password: {
+      value: '',
+      error: '',
+      isValid: true,
+    },
+  }
+
+  const { values, handleChange } = useForm(initialFormValues);
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+  }
   return (
-    <div className='Login'>
-      Login
-    </div>
+    <section className='login'>
+      <Logo />
+      <h2 className='login__title'>Рады видеть!</h2>
+      <AuthForm 
+        name='login'
+        submitText='Войти'
+        onSubmit={handleSubmit}
+        isValid={true}
+      >
+        
+        {/* <label for='login-name' className='auth-form__label'>Имя</label>
+        <input
+          className={`auth-form__input ${
+            !values.name.isValid && 'auth-form__input_invalid'
+          }`}
+          type='text'
+          name='name'
+          id='login-name'
+          placeholder='Введите имя'
+          minLength="2"
+          maxLength="30"
+          required
+          onChange={handleChange}
+          value={values.name.value}
+        />
+        <span
+          className={`auth-form__input-error ${
+            !values.name.isValid && 'auth-form__input-error_visible'
+          }`}>
+          {values.name.error}
+        </span> */}
+        <label for='login-email' className='auth-form__label'>E-mail</label>
+        <input
+          className={`auth-form__input ${
+            !values.email.isValid && 'auth-form__input_invalid'
+          }`}
+          type='email'
+          name='email'
+          id='login-email'
+          placeholder='Введите email'
+          required
+          onChange={handleChange}
+          value={values.email.value}
+        />
+        <span
+          className={`auth-form__input-error ${
+            !values.email.isValid && 'auth-form__input-error_visible'
+          }`}>
+          {values.email.error}
+        </span>
+        <label for='login-password' className='auth-form__label'>Пароль</label>
+        <input
+          className='auth-form__input'
+          type='password'
+          name='password'
+          id='login-password'
+          placeholder='Введите пароль'
+          required
+          onChange={handleChange}
+          value={values.password.value}
+        />
+        <span
+          className={`auth-form__input-error ${
+            !values.password.isValid && 'auth-form__input-error_visible'
+          }`}>
+          {values.password.error}
+        </span>        
+      </AuthForm>
+      <p className='login__question'>
+        Ещё не зарегистрированы? <Link to='/signup' className='login__link'>Регистрация</Link></p>      
+    </section>
   );
 }
 
