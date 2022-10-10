0334c414608198b287fdda041fa8a3df9e3114bf
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-10 13:27:10 +0500

    fix: footer tag and year constant

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,12 @@
 import './Footer.css';
 
 function Footer() {
+  const year = new Date().getFullYear();
   return (
-    <div className='footer'>
+    <footer className='footer'>
       <p className='footer__heading'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
       <div className='footer__contain'>
-        <p className='footer__copyright'>© { new Date().getFullYear() }</p>
+        <p className='footer__copyright'>© { year }</p>
         <ul className='footer__links'>
           <li>
             <a className='footer__link' href='https://practicum.yandex.ru' rel='noreferrer' target='_blank'>Яндекс.Практикум</a>
@@ -15,7 +16,7 @@ function Footer() {
           </li>
         </ul>
       </div>
-    </div> 
+    </footer> 
   );
 }
 
