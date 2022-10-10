a6174c6b4276cc006fa153ec9b01443c690c69ac
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-10 11:52:12 +0500

    feat: add auth form component

---
+++ src/components/AuthForm/AuthForm.js
@@ -0,0 +1,25 @@
+import './AuthForm.css';
+
+function AuthForm({ name, submitText, children, onSubmit, isValid }) {
+  return (
+    <form 
+      className='auth-form'
+      name={`${name}-form`}
+      onSubmit={onSubmit}
+      noValidate
+    >
+      {children}
+      <button
+        className={`auth-form__submit-button ${
+          !isValid && 'auth-form__submit-button_disabled'
+        }`}
+        type='submit'
+        disabled={!isValid}
+        aria-label={submitText}>
+        {submitText}
+      </button>
+    </form>
+  );
+}
+
+export default AuthForm;
