c9fcf449abaad6fc9958159ad2697917e9157cab
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-13 17:38:15 +0500

    fix: delete nav tab from promo

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,22 +1,16 @@
 import './Promo.css';
 
-import NavTab from '../NavTab/NavTab';
-
 import PromoImage from '../../images/promo.png';
 
 function Promo() {
   return (
-    <section className='promo'>
-      <NavTab />
-      <div className='promo__content'>
-        <div className='promo__text'>
-          <h1 className='promo__title'>Учебный проект студента факультета Веб&#8209;разработки.</h1>
-          <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-          <a href='#about' className='promo__link promo__link_type_next'>Узнать больше</a>
-        </div>
-        <img className='promo__image' src={PromoImage} alt='планета Земля' />
+    <section className='promo'>    
+      <div className='promo__text'>
+        <h1 className='promo__title'>Учебный проект студента факультета Веб&#8209;разработки.</h1>
+        <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
+        <a href='#about' className='promo__link promo__link_type_next'>Узнать больше</a>
       </div>
-      
+      <img className='promo__image' src={PromoImage} alt='планета Земля' />      
     </section>
   );
 }
