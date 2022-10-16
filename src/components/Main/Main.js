6405d4d1698a9a7f100c55726acc4d4a4ccd5b62
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-16 20:23:38 +0500

    fix: delete spaces

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,7 +2,6 @@ import './Main.css';
 
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer';
-
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from'../Techs/Techs';
