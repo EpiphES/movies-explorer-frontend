36812178259a6dbdfe57205569e569321f208d9b
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-16 11:21:47 +0500

    fix: allMovies array

--- src/utils/movies.js
+++ src/utils/movies.js
@@ -11,91 +11,123 @@ import movie10 from '../images/movie-images/movie10.jpg';
 import movie11 from '../images/movie-images/movie11.jpg';
 import movie12 from '../images/movie-images/movie12.jpg';
 
-const movies = [
+const allMovies = [
   {
     _id: 1,
-    nameRU: 'В погоне за Бенкси',
+    nameRU: 'В погоне за Бенкси самое длинное название фильма в мире',
     duration: 27,
-    image: movie1,    
-    saved: true,
+    image: movie1,  
   },
   {
     _id: 2,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
-    image: movie2,    
-    saved: true,
+    image: movie2,   
   },
   {
     _id: 3,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
     image: movie3,    
-    saved: false,
   },
   {
     _id: 4,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
     image: movie4,    
-    saved: false,
   },
   {
     _id: 5,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
     image: movie5,    
-    saved: false,
   },
   {
     _id: 6,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
-    image: movie6,    
-    saved: true,
+    image: movie6,
   },
   {
     _id: 7,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
-    image: movie7,    
-    saved: true,
+    image: movie7,
   },
   {
     _id: 8,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
-    image: movie8,    
-    saved: false,
+    image: movie8, 
   },
   {
     _id: 9,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
-    image: movie9,    
-    saved: false,
+    image: movie9,
   },
   {
     _id: 10,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
-    image: movie10,    
-    saved: false,
+    image: movie10,
   },
   {
     _id: 11,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
-    image: movie11,    
-    saved: true,
+    image: movie11,  
   },
   {
     _id: 12,
     nameRU: 'В погоне за Бенкси',
     duration: 27,
-    image: movie12,    
-    saved: false,
+    image: movie12, 
+  },
+  {
+    _id: 13,
+    nameRU: 'В погоне за Бенкси',
+    duration: 27,
+    image: movie12,
   },
 ]
 
-export default movies;
+const savedMovies = [
+  {
+    _id: 1,
+    nameRU: 'В погоне за Бенкси',
+    duration: 27,
+    image: movie1,
+    movieId: 1,  
+  },
+  {
+    _id: 2,
+    nameRU: 'В погоне за Бенкси',
+    duration: 27,
+    image: movie2,
+    movieId: 2,   
+  },
+  {
+    _id: 3,
+    nameRU: 'В погоне за Бенкси',
+    duration: 27,
+    image: movie6,
+    movieId: 6,
+  },
+  {
+    _id: 4,
+    nameRU: 'В погоне за Бенкси',
+    duration: 27,
+    image: movie7,
+    movieId: 7,
+  },
+  {
+    _id: 5,
+    nameRU: 'В погоне за Бенкси',
+    duration: 27,
+    image: movie11,
+    movieId: 1,  
+  },
+]
+
+export { allMovies, savedMovies };
