acf4634ab829d05d31ad910545db06df2c023c6a
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-16 20:19:46 +0500

    fix: code style

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,5 +1,6 @@
 import './AboutMe.css';
- import Avatar from '../../images/avatar.jpg';
+
+import Photo from '../../images/avatar.jpg';
 
 function AboutMe() {
   return (
@@ -12,11 +13,20 @@ function AboutMe() {
           <p className='about-me__description'>
             Я родилась и живу в Екатеринбурге, закончила лечебный факультет УГМА. Люблю спорт, занимаюсь скалолазанием и плаванием. Более 10 лет проработала врачом-рентгенологом. Недавно начала кодить. После того, как прошла курс по веб-разработке, ушла с постоянной работы.
           </p>
-          <a className='about-me__link' href='https://github.com/EpiphES' rel='noreferrer' target='_blank'>
+          <a 
+            className='about-me__link' 
+            href='https://github.com/EpiphES' 
+            rel='noreferrer' 
+            target='_blank'
+          >
             Github
           </a>    
         </div>
-        <img src={Avatar} className='about-me__photo' alt='мое фото' />   
+        <img
+          className='about-me__photo' 
+          src={Photo}
+          alt='мое фото'
+        />   
       </div>
     </section>
   );
