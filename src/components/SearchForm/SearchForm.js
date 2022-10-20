8be31540e0ace20026764a048bdb947d668f11cd
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-20 16:43:49 +0500

    feat: enable search and filer checkbox, add search input error

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,35 +1,66 @@
 import './SearchForm.css';
 
+import { useState } from 'react';
+
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
-function SearchForm({ name }) {
+function SearchForm({ name, handleFilterMovies }) {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchQueryError, setSearchQueryError] = useState('');
+  const [isFilterActive, setFilterActive] = useState(false);
+
+  function  handleCheckBox() {
+      setFilterActive((prevState) => !prevState);    
+    }
+
+  function handleSearchInputChange(evt) {
+    setSearchQuery(evt.target.value);
+    setSearchQueryError('');
+  }  
+
   function handleSubmit(evt) {
-    evt.preventDefault();    
+    evt.preventDefault();
+    searchQuery ? 
+      handleFilterMovies(searchQuery, isFilterActive)
+      : 
+      setSearchQueryError('Нужно ввести ключевое слово');   
   }
+
   return (
     <section className="search-form">
       <form 
         className='search-form__form' 
         name={`${name}-form`}
         onSubmit={ handleSubmit }
-        // noValidate
+        noValidate
       >
         <div className='search-form__input-group'>
           <input
-            className='search-form__input'
+            className={`search-form__input ${searchQueryError && 'search-form__input_invalid'}`}
             type='text'
             name='search'
             placeholder='Фильм'
             required
             autoFocus
-            // onChange={ handleChange }
-            // value={ values.search.value }           
+            onChange={handleSearchInputChange}
+            value={searchQuery}           
           />
-          <button className='search-form__submit-button'
-          // disabled={ values.search.value.length === 0 } 
-          type="submit" />            
+          <button 
+            className='search-form__submit-button'
+            type='submit'
+            aria-label='начать поиск' 
+          />            
         </div>
-        <FilterCheckbox />
+        <span
+          className={`search-form__input-error ${
+            searchQueryError && 'auth-form__input-error_visible'
+          }`}>
+          {searchQueryError}
+        </span>
+        <FilterCheckbox 
+          isChecked={isFilterActive}
+          handleCheckBox={handleCheckBox}
+        />
       </form>
       
     </section>
