68a479672a57bcafabf7439569794e5725f4c312
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-22 17:24:06 +0500

    feat: enable search function for saved movies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,22 +1,46 @@
 import './SavedMovies.css';
 
+import { useState, useEffect } from 'react';
+
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import SearchForm from '../SearchForm/SearchForm';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
 
-function SavedMovies({ movies, loggedIn, handleDeleteMovie }) {
+import { filterMovies } from '../../utils/utils';
+
+function SavedMovies({ savedMovies, loggedIn, handleDeleteMovie, isError }) {
+  const [searchedMovies, setSearchedMovies] = useState([]);
+
+  function handleSearch(searchQuery, isFilterActive) {
+    setSearchedMovies(filterMovies(savedMovies, searchQuery, isFilterActive));
+  }
+
+  useEffect(() => {
+    setSearchedMovies(savedMovies);
+  }, [savedMovies]);
 
   return (
     <>
       <Header page={'saved-movies'} loggedIn={loggedIn}/>
       <main className='saved-movies'>
-        <SearchForm />
-        <MoviesCardList 
-          movies={movies}
-          isSavedMoviesPage={true}
-          handleDeleteMovie={handleDeleteMovie}
-        />     
+        <SearchForm 
+          name={'saved-movies'}
+          handleSearch={handleSearch}
+        />
+        { isError && <p className='movies__error-message'>Во время запроса произошла ошибка. Возможно, проблема с соединением или сервер недоступен. Подождите немного и попробуйте ещё раз.</p>}
+        {
+          !isError && searchedMovies.length === 0 &&
+          <p className='movies__error-message'>Ничего не найдено</p>
+        }
+        {
+          !isError && searchedMovies.length > 0 &&
+          <MoviesCardList 
+            movies={searchedMovies}
+            isSavedMoviesPage={true}
+            handleDeleteMovie={handleDeleteMovie}
+          />
+        }     
       </main>
       <Footer />
     </>
