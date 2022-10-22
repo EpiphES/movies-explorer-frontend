8fbf1710cd4e735d920f7277ac89a20a9ef73433
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-22 20:20:43 +0500

    fix: delete useCallback

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {useState, useEffect, useCallback } from 'react';
+import {useState, useEffect } from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 
 import Main from '../Main/Main';
