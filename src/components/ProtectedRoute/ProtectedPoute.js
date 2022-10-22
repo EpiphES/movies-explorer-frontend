a182410e8fb6cb09070e0061b8a34298d04aea1f
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-22 19:42:43 +0500

    fix: delete backspace

--- src/components/ProtectedRoute/ProtectedPoute.js
+++ src/components/ProtectedRoute/ProtectedPoute.js
@@ -1,6 +1,6 @@
 import { Navigate } from 'react-router-dom';
 
-function ProtectedRoute ({ component: Component, ...props }){
+function ProtectedRoute({ component: Component, ...props }) {
   return (
     props.loggedIn ? <Component {...props} /> : <Navigate to='/' />  
   );
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -40,8 +40,6 @@ function convertTime(minutes) {
   }  
 }
 
-
-
 export {
   filterByKeyWord, 
   filterByDuration,
