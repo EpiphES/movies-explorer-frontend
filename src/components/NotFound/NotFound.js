7e2811031111b6aa179464b9a6ff3f22b3382295
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-22 19:38:09 +0500

    feat: go back button for not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,15 +1,18 @@
 import './NotFound.css';
 
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function NotFound() {
+  const navigate = useNavigate();
+
+
   return (
     <div className='not-found'>
       <div className='not-found__content'>
         <h1 className='not-found__title'>404</h1>
         <p className='not-found__subtitle'>Страница не найдена</p>
       </div>
-    <Link to='/' className='not-found__link'>Назад</Link>     
+    <button type='button' aria-label='назад' className='not-found__go-back' onClick={() => navigate(-1)}>Назад</button>     
     </div>
   );
 }
