b07843c5dfcd759c3fee6916bbbba4ad72503896
Ekaterina Epiphanova <epiph@yandex.ru>
2022-10-27 16:57:27 +0500

    fix: promo__link class name

--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -8,7 +8,7 @@ function Promo() {
       <div className='promo__text'>
         <h1 className='promo__title'>Учебный проект студента факультета Веб&#8209;разработки.</h1>
         <p className='promo__subtitle'>Листайте ниже, чтобы узнать больше про этот проект и его создателя.</p>
-        <a href='#about' className='promo__link promo__link_type_next'>Узнать больше</a>
+        <a href='#about' className='promo__link'>Узнать больше</a>
       </div>
       <img className='promo__image' src={PromoImage} alt='планета Земля' />      
     </section>
